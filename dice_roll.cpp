0ada4e107ad83fe8e86448f5c2198a872d996a0e
BaNavyBlue <abajor@ucsc.edu>
2024-01-15 17:49:50 -0800

    removing the old

--- dice_roll.cpp
+++
@@ -1,8 +0,0 @@
-#include "dice_roll.h"
-
-unsigned int rollDice(unsigned char sides, bool zeroLowest){
-    //srand((unsigned int) time(NULL));
-    unsigned char offByOne = 1;
-    if(zeroLowest) offByOne = 0;
-    return rand()%sides + offByOne;
-}
